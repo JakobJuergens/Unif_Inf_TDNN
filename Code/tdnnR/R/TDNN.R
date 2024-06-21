d422edd7842e8fb1d3b80fe9bfaccf89921a9b4b
Jakob Juergens <jakob.r.juergens@gmail.com>
2024-06-21 11:10:37 -0500

    Package Description

---
+++ Code/tdnnR/R/TDNN.R
@@ -0,0 +1,28 @@
+#' Calculate the distributional nearest neighbor estimate for a point x
+#' based on a sample data with subsampling scale s.
+#' The implementation is based on the equivalent represetation as an L-statistic
+#' described in Steele (2009)
+#'
+#' @param x The point of interest.
+#' @param data The data set containing the observations.
+#' @param s The subsampling scale.
+#' @return A number.
+#'
+DNN <- function(x, data, s){
+
+}
+
+#' Calculate the distributional nearest neighbor estimate for a point x
+#' based on a sample data with subsampling scale s.
+#' The implementation is based on the equivalent represetation as an L-statistic
+#' described in Steele (2009)
+#'
+#' @param x The point of interest.
+#' @param data The data set containing the observations.
+#' @param s1 The first subsampling scale.
+#' @param s2 The second subsampling scale.
+#' @return A number.
+#'
+TDNN <- function(x, data, s){
+
+}
