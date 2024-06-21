1c5d2a2da851771bc28e58026ee2e665cb5d56d2
Jakob Juergens <jakob.r.juergens@gmail.com>
2024-06-21 14:12:41 -0500

    DNN start implementation

--- Code/tdnnR/R/TDNN.R
+++ Code/tdnnR/R/TDNN.R
@@ -10,13 +10,31 @@
 #'
 #' @export
 DNN <- function(x, data, s){
+  # Check inputs for executing function
+  point_check(x)
+  data_check(data)
+  data_point_compatibility_check(x, data)
 
+  # Pre-sort data relative to point of interest
+  data_sorted <- pre_sort(x = x, data = data)
+  n <- nrow(data_sorted)
+  d <- ncol(data_sorted) - 1
+
+  # Calculate DNN estimator
+  res <- 0
+  factor <- 1
+  prefactor <- 1/choose(n,s)
+  for(i in 1:(n-s+1)){
+    index <- n-s+2-i
+    res <- res + factor*data_sorted[index,1]
+    factor <- factor*((n-index+1)/(n - index - s + 2))
+  }
 }
 
 #' Calculate the distributional nearest neighbor estimate for a point x
 #' based on a sample data with subsampling scale s.
 #' The implementation is based on the equivalent representation as an L-statistic
-#' described in Steele (2009) and Demirkaya et al. (2024)
+#' described in Steele (2009) and the TDNN estimator from Demirkaya et al. (2024)
 #'
 #' @param x The point of interest.
 #' @param data The data set containing the observations.
@@ -26,7 +44,17 @@ DNN <- function(x, data, s){
 #'
 #' @export
 TDNN <- function(x, data, s1, s2){
+  # Check inputs for executing function
+  point_check(x)
+  data_check(data)
+  data_point_compatibility_check(x, data)
 
+  # Pre-sort data relative to point of interest
+  data_sorted <- pre_sort(x = x, data = data)
+
+  # Calculate weights for two-scale DNN
+  w1 <- 0.5
+  w2 <- 1 - w_1
 }
 
 
---
+++ Code/tdnnR/R/preChecks.R
@@ -0,0 +1,58 @@
+#' Check the provided data for correct format etc
+#'
+#' @param data The data set containing the observations.
+#' @return TRUE or FALSE
+data_check <- function(data){
+  if(1 == 0){
+    stop('Error 1 occured.')
+  }
+  return(TRUE)
+}
+
+#' Check the provided point of interest for correct format etc
+#'
+#' @param x The point of interest
+#' @return TRUE or FALSE
+point_check <- function(x){
+  if(1 == 0){
+    stop('Error 1 occured.')
+  }
+  return(TRUE)
+}
+
+#' Check the provided data and point of interest for compatibility
+#'
+#' @param x The point of interest
+#' @param data The data set containing the observations.
+#' @return TRUE or FALSE
+data_point_compatibility_check <- function(x, data){
+  if(1 == 0){
+    stop('Error 1 occured.')
+  }
+  return(TRUE)
+}
+
+#' Check the provided data and sampling scale of interest for compatibility
+#'
+#' @param s The subsampling scale
+#' @param data The data set containing the observations.
+#' @return TRUE or FALSE
+samplingscale_check <- function(s, data){
+  if(1 == 0){
+    stop('Error 1 occured.')
+  }
+  return(TRUE)
+}
+
+#' Check the provided data and subsampling scales for compatibility
+#'
+#' @param s1 The first subsampling scale
+#' @param s2 The second subsampling scale
+#' @param data The data set containing the observations.
+#' @return TRUE or FALSE
+samplingscale_check2 <- function(s1, s2, data){
+  if(1 == 0){
+    stop('Error 1 occured.')
+  }
+  return(TRUE)
+}
