98ea43a1128b63a31a630da7f0edf407f92f5117
Jakob Juergens <jakob.r.juergens@gmail.com>
2024-06-21 15:52:58 -0500

    Package Update

--- Code/tdnnR/R/TDNN.R
+++ Code/tdnnR/R/TDNN.R
@@ -9,16 +9,20 @@
 #' @return A number.
 #'
 #' @export
-DNN <- function(x, data, s){
+DNN <- function(x, data, s, presorted = FALSE){
   # Check inputs for executing function
   point_check(x)
   data_check(data)
   data_point_compatibility_check(x, data)
+  samplingscale_check(s, data)
 
   # Pre-sort data relative to point of interest
-  data_sorted <- pre_sort(x = x, data = data)
-  n <- nrow(data_sorted)
-  d <- ncol(data_sorted) - 1
+  if(presorted == FALSE){
+    data <- pre_sort(x = x, data = data)
+  }
+
+  n <- nrow(data)
+  d <- ncol(data) - 1
 
   # Calculate DNN estimator
   res <- 0
@@ -26,9 +30,13 @@ DNN <- function(x, data, s){
   prefactor <- 1/choose(n,s)
   for(i in 1:(n-s+1)){
     index <- n-s+2-i
-    res <- res + factor*data_sorted[index,1]
+    res <- res + factor*data[index,1]
     factor <- factor*((n-index+1)/(n - index - s + 2))
   }
+  res <- prefactor*res
+
+  # return calculated result
+  return(res)
 }
 
 #' Calculate the distributional nearest neighbor estimate for a point x
@@ -43,18 +51,58 @@ DNN <- function(x, data, s){
 #' @return A number.
 #'
 #' @export
-TDNN <- function(x, data, s1, s2){
+TDNN <- function(x, data, s1, s2, presorted = FALSE){
   # Check inputs for executing function
   point_check(x)
   data_check(data)
   data_point_compatibility_check(x, data)
+  samplingscale_check2(s1, s2, data)
 
   # Pre-sort data relative to point of interest
-  data_sorted <- pre_sort(x = x, data = data)
+  if(presorted == FALSE){
+    data <- pre_sort(x = x, data = data)
+  }
+
+  # Check that s2 > s1 and reverse if not
+  if(s1 > s2){
+    tmp <- s1
+    s_1 <- s2
+    s_2 <- tmp
+  }
 
   # Calculate weights for two-scale DNN
   w1 <- 0.5
   w2 <- 1 - w_1
+
+  # Calculate the two DNN estimators
+  res1 <- 0
+  res2 <- 0
+  factor1 <- 1
+  factor2 <- 1
+  prefactor1 <- 1/choose(n,s1)
+  prefactor2 <- 1/choose(n,s2)
+
+  for(i in 1:(s2-s1)){
+    index <- n-s2+2-i
+    res2 <- res2 + factor2*data[index,1]
+    factor2 <- factor2*((n-index+1)/(n - index - s2 + 2))
+  }
+
+  for(i in 1:(n-s1+1)){
+    index <- n-s1+2-i
+
+    res1 <- res1 + factor1*data[index,1]
+    res2 <- res2 + factor2*data[index,1]
+
+    factor1 <- factor1*((n-index+1)/(n - index - s1 + 2))
+    factor2 <- factor2*((n-index+1)/(n - index - s2 + 2))
+  }
+
+  res1 <- prefactor1*res1
+  res2 <- prefactor2*res2
+
+  # Combine results
+
 }
 
 
